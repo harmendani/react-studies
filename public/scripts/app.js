04ac10e5501673880c7454d628908f5c1f3fd67a
Pedro Harmendani <harmendani26@gmail.com>
2020-01-19 19:59:30 -0300

    Add gitignore Add package npm

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,14 +3,14 @@
 console.log('React is running...');
 
 // JSX
-// <p>This is an App build with React!</p>
+var template = <p>This is an App build with React!</p>;
 
-// Create an element
+/*// Create an element with native js calls
 var template = React.createElement(
-'p',
-{id: 'someId'},
-'This is an App build with React!'
-)
+    'p',
+    { id: 'someId' },
+    'This is an App build with React!'
+)*/
 
 // Get element by id
 var appRoot = document.getElementById('app');
