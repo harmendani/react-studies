a1d4ceb8d165917842266477e34268e837de43ad
Pedro Harmendani <harmendani26@gmail.com>
2020-02-02 21:04:56 -0300

    Created nested elements with JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,8 @@
 'use strict';
 
-console.log('React is running...'); // JSX
+console.log('React is running...'); // JSX Sintax
 
-var template = React.createElement("h1", null, "Hello World @Brazil!");
-/*// Create an element with native js calls
-var template = React.createElement(
-    'p',
-    { id: 'someId' },
-    'This is an App build with React!'
-)*/
-// Get element by id
+var template = React.createElement("div", null, React.createElement("h1", null, "Hello World @Brazil!"), React.createElement("p", null, "There are new info today!"), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two"))); // Get element by id
 
 var appRoot = document.getElementById('app'); // Render element
 
--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@
 
 console.log('React is running...');
 
-// JSX
-var template = <h1>Hello World @Brazil!</h1>;
-
-/*// Create an element with native js calls
-var template = React.createElement(
-    'p',
-    { id: 'someId' },
-    'This is an App build with React!'
-)*/
+// JSX Sintax
+var template = (
+    <div>
+        <h1>Hello World @Brazil!</h1>
+        <p>There are new info today!</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
 
 // Get element by id
 var appRoot = document.getElementById('app');
