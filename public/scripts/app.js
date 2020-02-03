db1eb6aeb12295dafca7555464c2595fcebffa05
Pedro Harmendani <harmendani26@gmail.com>
2020-02-02 21:10:15 -0300

    Format code on app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,6 @@
 
 console.log('React is running...'); // JSX Sintax
 
-var template = React.createElement("div", null, React.createElement("h1", null, "Hello World @Brazil!"), React.createElement("p", null, "There are new info today!"), React.createElement("ol", null, React.createElement("li", null, "Item One"), React.createElement("li", null, "Item Two")));
 var templateTwo = React.createElement("div", null, React.createElement("h1", null, "Pedro Harmendani"), React.createElement("p", null, "Age: 24"), React.createElement("p", null, "Location: Minas Gerais, Brazil.")); // Get element by id
 
 var appRoot = document.getElementById('app'); // Render element
